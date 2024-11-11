9424e19ceaa44410173d0634cdbe79be2e513b37
Ljubica Jovanoska <lubicaj724@gmail.com>
2024-11-11 13:25:55 +0100

    Finished Lab1 - bonus

--- src/main/java/mk/finki/ukim/web/lab/model/Event.java
+++ src/main/java/mk/finki/ukim/web/lab/model/Event.java
@@ -5,14 +5,23 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Data
-@AllArgsConstructor
 public class Event {
     private  String name;
     private String description;
     private double popularityScore;
     private int ticketCount;
+    private Long id;
+    private Location location;
 
-
+    public Event(String name, String description, double popularityScore, int ticketCount) {
+        this.name = name;
+        this.description = description;
+        this.popularityScore = popularityScore;
+        this.ticketCount = ticketCount;
+        Random random = new Random();
+        this.id = random.nextLong();
+    }
 }

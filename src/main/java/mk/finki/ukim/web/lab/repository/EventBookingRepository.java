3e63d2de8e0308fea8e28f6869acbd633cc4b657
Ljubica Jovanoska <lubicaj724@gmail.com>
2024-11-12 22:51:30 +0100

    Finished Lab2 - name not showing

--- src/main/java/mk/finki/ukim/web/lab/repository/EventBookingRepository.java
+++ src/main/java/mk/finki/ukim/web/lab/repository/EventBookingRepository.java
@@ -1,19 +1,24 @@
 package mk.finki.ukim.web.lab.repository;
 
-
-
 import mk.finki.ukim.web.lab.model.EventBooking;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
-public class EventBookingRepository{
-    public List<EventBooking> bookings=new ArrayList<>();
-    public List<EventBooking> findAll(){
-        return bookings;
+public class EventBookingRepository {
+    public final List<EventBooking> bookings = new ArrayList<>();
+
+    public void save(EventBooking booking) {
+        bookings.add(booking);
     }
 
+    public List<EventBooking> findAll() {
+        return new ArrayList<>(bookings);
+    }
+    // New method to add
+    public List<EventBooking> getAllBookings() {
+        return new ArrayList<>(bookings);
+    }
 }
